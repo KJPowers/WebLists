866f687cf0da2d0ef0715e7280a70fd4b48cfb24
Keith <K.J.Powers@gmail.com>
2022-04-25 23:50:32 -0400

    Get everything working from a class instead of a specially constructed array.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require_once 'models/Models.php';
 
 $opts = ['extension' => 'html'];
 
@@ -10,28 +11,20 @@ $m = new Mustache_Engine(array(
   'pragmas'          => [Mustache_Engine::PRAGMA_ANCHORED_DOT],
   'strict-callables' => 'true',
 ));
-//echo $m->render('index', array('planet' => 'World!')); // "Hello World!"
-/*echo $m->render('test', [
-  'title' => 'my links',
-  'links' => [
-    ['href' => 'https://mustache.github.io'],
-    ['href' => 'https://github.com/mustache/spec', 'title' => 'The Mustache spec'],
-    ['href' => 'https://github.com/bobthecow/mustache.php', 'title' => 'Mustache.php'],
-] ]);
-*/
 
-$vars = [
-  'lists' => array(
-    ['class' => 'nav-link', 'name' => '<First List>'],
-    ['class' => 'nav-link', 'name' => '<Second List>'],
-    ['class' => 'nav-link', 'name' => '<Third List>'],
-    ['class' => 'nav-link', 'name' => '<Fourth List>'],
-  ),
- 'items' => array(
-    ['class' => 'nav-link', 'name' => 'Item one'],
-    ['class' => 'nav-link', 'name' => 'Item two'],
-    ['class' => 'nav-link', 'name' => 'Item three'],
-  ),
-];
+$mdl = new Index();
+$mdl->lists = array(
+    new NavbarWebList('abc', '<List 1>', 'The first list'),
+    new NavbarWebList('def', '<List 2>', 'The second list'),
+    new NavbarWebList('ghi', '<List 3>', 'The third list'),
+  );
+$mdl->items = array(
+    new NavbarItem(1, '<Item 1>', 'foo'),
+    new NavbarItem(2, '<Item 2>', ''),
+    new NavbarItem(3, '<Item 3>', ''),
+  );
 
-echo $m->render('index', $vars);
+echo $m->render('index', $mdl);
+
+// debug
+//echo var_dump($mdl);
---
+++ models/Models.php
@@ -0,0 +1,65 @@
+<?php
+class Index
+{
+  public $lists;
+  public $items;
+}
+
+class WebList
+{
+  private ?string $uuid;
+  public  ?string $name;
+  public  ?string $description;
+
+  public function __construct(?string $uuid = null, ?string $name = null, ?string $description = null)
+  {
+    $this->uuid = $uuid;
+    $this->name = $name;
+    $this->description = $description;
+  }
+
+  public function uuid(): string
+  {
+    return $this->$uuid;
+  }
+}
+
+class NavbarWebList extends WebList
+{
+  public ?string $class = 'nav-link';
+
+  public function __construct(?string $uuid = null, ?string $name = null, ?string $description = null)
+  {
+    parent::__construct($uuid, $name, $description);
+  }
+
+  public function id(): int
+  {
+    return $this->$id;
+  }
+}
+
+class Item
+{
+  private ?int    $id;
+  public ?string $name;
+  public ?string $description;
+
+  public function __construct(?int $id = null, ?string $name = null, ?string $description = null)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->description = $description;
+  }
+}
+
+class NavbarItem extends Item
+{
+  public ?string $class = 'nav-link';
+
+  public function __construct(?int $id = null, ?string $name = null, ?string $description = null)
+  {
+    parent::__construct($id, $name, $description);
+  }
+}
+
